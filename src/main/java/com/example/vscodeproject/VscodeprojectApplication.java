692af60d512e5759d6bc6e2ed155b651ffb35647
devchoi <cnhhw0408@gmail.com>
2025-05-07 09:27:33 +0900

    init commit

---
+++ src/main/java/com/example/vscodeproject/VscodeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.vscodeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VscodeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VscodeprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vscodeproject/VscodeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vscodeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VscodeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
